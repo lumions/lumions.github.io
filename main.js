59f274cbef79a539d20c5494daaac710f5d7279f
lumions <30581693+lumions@users.noreply.github.com>
2017-08-04 16:49:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ var links = ["http://rs.sfacg.com/web/novel/images/NovelCover/Big/e36f7fa4-01e4-
 
 "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/151010_Sapporo_ramen_at_Susukino_Sapporo_Hokkaido_Japan01s.jpg/270px-151010_Sapporo_ramen_at_Susukino_Sapporo_Hokkaido_Japan01s.jpg",
 
-"http://thehub.musiciansfriend.com/images/drumsbuying/pearl-reference-pure-standard-natural-drum-set.jpg", 
-             "http://vignette3.wikia.nocookie.net/sakurasounopetnakanojo/images/4/4f/Shiinachan.jpg/revision/latest?cb=20150201181958"
+"http://thehub.musiciansfriend.com/images/drumsbuying/pearl-reference-pure-standard-natural-drum-set.jpg",
+             "http://vignette3.wikia.nocookie.net/sakurasounopetnakanojo/images/4/4f/Shiinachan.jpg/revision/latest?cb=20150201181958",
+             
 ];
-var titles = [];
+
+var titles = ["Traxxas Slash 4X4 遙控車", "Traxxas Bandit 2wd 遙控車", "四軸直昇機", "3D設計及列印", "Python程式設計", "Scratch兒童程式教學", "AI人工智慧"];
 var index = 0;
 var autoplay = true;
 
@@ -18,18 +20,17 @@ function nextPic() {
   console.log("nextPic: " + index);
   var img = document.getElementById("pic");
   index++;
-  img.src = links[index % links.legnth];
-  
+  img.src = links[index % links.length];
+  console.log(links)
   var title = document.getElementById("title");
   title.innerHTML = titles[index % links.length];
   console.log(title);
+  
   if(autoplay) {
     setTimeout(nextPic, 3000);
-}
-
+  }
 }
 
 if(autoplay) {
     setTimeout(nextPic, 3000);
-  }
-
+}
